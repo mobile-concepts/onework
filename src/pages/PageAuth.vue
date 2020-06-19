d5ab7e3da51703b87330bf7973cf2f8375ec1dde
mcjazzyfunky <mcjazzyfunky@users.noreply.github.com>
2020-06-19 22:22:50 +0200

    Fixed CSS issues

--- src/pages/PageAuth.vue
+++ src/pages/PageAuth.vue
@@ -3,183 +3,204 @@
     <q-header elevated>
       <q-toolbar>
         <q-icon name="people_alt" size="md" />
-        <q-toolbar-title> OneWork &middot; <b>Sign in</b> </q-toolbar-title>
+        <q-toolbar-title>
+          OneWork &middot;
+          <span>Sign in</span>
+        </q-toolbar-title>
       </q-toolbar>
     </q-header>
+
     <q-page-container>
       <q-page padding>
-        <q-card>
-          <q-tabs
-            v-model="activeTab"
-            dense
-            class="text-grey"
-            active-color="primary"
-            indicator-color="primary"
-            align="justify"
-            narrow-indicator
-          >
-            <q-tab name="login" label="Login" />
-            <q-tab name="forgotPassword" label="Forgot Password" />
-            <q-tab name="register" label="Register" />
-          </q-tabs>
+        <div class="scrollpane">
+          <q-card class="main-card">
+            <q-tabs
+              v-model="activeTab"
+              dense
+              class="text-grey"
+              active-color="primary"
+              indicator-color="primary"
+              align="justify"
+              narrow-indicator
+            >
+              <q-tab name="login" label="Login" />
+              <q-tab name="forgotPassword" label="Forgot Password" />
+              <q-tab name="register" label="Register" />
+            </q-tabs>
 
-          <q-separator />
+            <q-separator />
 
-          <q-tab-panels v-model="activeTab" animated>
-            <q-tab-panel name="login" class="q-gutter-sm">
-              <div class="text-h6">Login user</div>
-              <q-banner class="bg-grey-3">
-                <template v-slot:avatar>
-                  <q-icon name="account_circle" color="primary" />
-                </template>
-                Welcome to OneWork. Please enter your credentials to log in.
-              </q-banner>
-              <q-input outlined v-model="login.username" label="Username" />
-              <q-input
-                outlined
-                v-model="login.organization"
-                label="Organization"
-              />
-              <div class="text-right">
-                <a class="text-primary link" @click="setActiveTab('register')">
-                  Not yet registered?
-                </a>
-              </div>
-              <q-input
-                type="password"
-                outlined
-                v-model="password"
-                label="Password"
-              />
-              <div class="text-right">
-                <a
-                  class="text-primary link float-right"
-                  @click="setActiveTab('forgotPassword')"
-                >
-                  Forgotten your password?
-                </a>
-              </div>
-              <q-checkbox v-model="login.remember">
-                Remember login
-              </q-checkbox>
-              <div class="row">
-                <q-space />
-                <q-btn color="primary" label="Log in" />
-              </div>
-            </q-tab-panel>
-
-            <q-tab-panel name="forgotPassword" class="q-gutter-sm">
-              <div class="text-h6">Forgot password</div>
-              <q-banner class="bg-grey-3">
-                <template v-slot:avatar>
-                  <q-icon name="lock_open" color="primary" />
-                </template>
-                If forgotten your password then reset it by following the
-                instructions that will be sent to you by email.
-              </q-banner>
-              <q-input outlined v-model="username" label="Username" />
-              <q-input outlined v-model="organization" label="Organization" />
-              <div class="row">
-                <q-space />
-                <q-btn color="primary" label="Reset password" />
-              </div>
-            </q-tab-panel>
-
-            <q-tab-panel name="register" class="q-gutter-sm">
-              <div class="text-h6">Register organization</div>
-              <q-banner class="bg-grey-3">
-                <template v-slot:avatar>
-                  <q-icon name="domain" color="primary" />
-                </template>
-                Please register your organization if not already done. Otherwise
-                go to the
-                <a
-                  @click.prevent="setActiveTab('login')"
-                  class="text-primary link"
-                >
-                  Login Page
-                </a>
-              </q-banner>
-              <div class="text-h7">Organization</div>
-              <q-input outlined v-model="register.organization.id" label="ID" />
-              <q-input
-                outlined
-                v-model="register.organization.name"
-                label="Organization name"
-              />
-              <q-input
-                outlined
-                v-model="register.organization.street"
-                label="Street"
-              />
-              <div class="row">
+            <q-tab-panels v-model="activeTab" animated>
+              <q-tab-panel name="login" class="q-gutter-sm">
+                <div class="text-h6">Login user</div>
+                <q-banner class="bg-grey-3">
+                  <template v-slot:avatar>
+                    <q-icon name="account_circle" color="primary" />
+                  </template>
+                  Welcome to OneWork. Please enter your credentials to log in.
+                </q-banner>
+                <q-input outlined v-model="login.username" label="Username*" />
+                <div class="text-right">
+                  <a
+                    class="text-primary link"
+                    @click="setActiveTab('register')"
+                  >
+                    No account yet?
+                  </a>
+                </div>
                 <q-input
                   outlined
-                  v-model="register.zip"
-                  label="Zip"
-                  class="q-mr"
+                  v-model="login.organization"
+                  label="Organization"
+                />
+                <div class="text-right">
+                  <a
+                    class="text-primary link"
+                    @click="setActiveTab('register')"
+                  >
+                    Not yet registered?
+                  </a>
+                </div>
+                <q-input
+                  type="password"
+                  outlined
+                  v-model="password"
+                  label="Password"
+                />
+                <div class="text-right">
+                  <a
+                    class="text-primary link float-right"
+                    @click="setActiveTab('forgotPassword')"
+                  >
+                    Forgotten your password?
+                  </a>
+                </div>
+                <q-checkbox v-model="login.remember">
+                  Remember login
+                </q-checkbox>
+                <div class="row">
+                  <q-space />
+                  <q-btn color="primary" label="Log in" />
+                </div>
+              </q-tab-panel>
+
+              <q-tab-panel name="forgotPassword" class="q-gutter-sm">
+                <div class="text-h6">Forgot password</div>
+                <q-banner class="bg-grey-3">
+                  <template v-slot:avatar>
+                    <q-icon name="lock_open" color="primary" />
+                  </template>
+                  If forgotten your password then reset it by following the
+                  instructions that will be sent to you by email.
+                </q-banner>
+                <q-input outlined v-model="username" label="Username" />
+                <q-input outlined v-model="organization" label="Organization" />
+                <div class="row">
+                  <q-space />
+                  <q-btn color="primary" label="Reset password" />
+                </div>
+              </q-tab-panel>
+
+              <q-tab-panel name="register" class="q-gutter-sm">
+                <div class="text-h6">Register organization</div>
+                <q-banner class="bg-grey-3">
+                  <template v-slot:avatar>
+                    <q-icon name="domain" color="primary" />
+                  </template>
+                  Please register your organization if not already done.
+                  Otherwise go to the
+                  <a
+                    @click.prevent="setActiveTab('login')"
+                    class="text-primary link"
+                  >
+                    Login Page
+                  </a>
+                </q-banner>
+                <div class="text-h7">Organization</div>
+                <q-input
+                  outlined
+                  v-model="register.organization.id"
+                  label="ID"
                 />
                 <q-input
                   outlined
-                  v-model="register.organization.city"
-                  label="City"
+                  v-model="register.organization.name"
+                  label="Organization name"
                 />
-              </div>
-              <q-select
-                outlined
-                v-model="register.organization.country"
-                :options="availableCountries"
-                label="Country"
-              />
-              <q-input
-                type="password"
-                outlined
-                v-model="password"
-                label="Password"
-              />
+                <q-input
+                  outlined
+                  v-model="register.organization.street"
+                  label="Street"
+                />
+                <div class="row">
+                  <q-input
+                    outlined
+                    v-model="register.zip"
+                    label="Zip"
+                    class="q-mr"
+                  />
+                  <q-input
+                    outlined
+                    v-model="register.organization.city"
+                    label="City"
+                  />
+                </div>
+                <q-select
+                  outlined
+                  v-model="register.organization.country"
+                  :options="availableCountries"
+                  label="Country"
+                />
+                <q-input
+                  type="password"
+                  outlined
+                  v-model="password"
+                  label="Password"
+                />
 
-              <div class="text-h7">Admin user</div>
-              <q-input
-                outlined
-                v-model="register.adminUser.username"
-                label="Username"
-              />
-              <q-input
-                outlined
-                v-model="register.adminUser.firstName"
-                label="First name"
-              />
-              <q-input
-                outlined
-                v-model="register.adminUser.lastName"
-                label="Last name"
-              />
-              <q-input
-                outlined
-                v-model="register.adminUser.lastName"
-                label="E-mail"
-              />
-              <q-input
-                outlined
-                v-model="register.adminUser.password"
-                label="Password"
-              />
-              <q-input
-                outlined
-                v-model="register.adminUser.password2"
-                label="Repeat password"
-              />
-              <q-checkbox v-model="register.termsAndConditions.accept">
-                Accept <a class="text-primary link">Terms and Conditions</a>
-              </q-checkbox>
+                <div class="text-h7">Admin user</div>
+                <q-input
+                  outlined
+                  v-model="register.adminUser.username"
+                  label="Username"
+                />
+                <q-input
+                  outlined
+                  v-model="register.adminUser.firstName"
+                  label="First name"
+                />
+                <q-input
+                  outlined
+                  v-model="register.adminUser.lastName"
+                  label="Last name"
+                />
+                <q-input
+                  outlined
+                  v-model="register.adminUser.lastName"
+                  label="E-mail"
+                />
+                <q-input
+                  outlined
+                  v-model="register.adminUser.password"
+                  label="Password"
+                />
+                <q-input
+                  outlined
+                  v-model="register.adminUser.password2"
+                  label="Repeat password"
+                />
+                <q-checkbox v-model="register.termsAndConditions.accept">
+                  Accept <a class="text-primary link">Terms and Conditions</a>
+                </q-checkbox>
 
-              <div class="row">
-                <q-space />
-                <q-btn color="primary" label="Register and log in" />
-              </div>
-            </q-tab-panel>
-          </q-tab-panels>
-        </q-card>
+                <div class="row">
+                  <q-space />
+                  <q-btn color="primary" label="Register and log in" />
+                </div>
+              </q-tab-panel>
+            </q-tab-panels>
+          </q-card>
+        </div>
       </q-page>
     </q-page-container>
   </q-layout>
@@ -236,8 +257,32 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .link {
   cursor: pointer;
+}
+
+.scrollpane {
+  overflow: auto;
+  margin: 0;
+  padding: 16px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  height: 100%;
+  max-height: 100%;
+  width: 100%;
+  max-width: 100%;
+}
+
+.main-card {
+  max-width: 768px;
+  margin: 0 auto;
+
+  * {
+    overflow: hidden;
+  }
 }
 </style>
